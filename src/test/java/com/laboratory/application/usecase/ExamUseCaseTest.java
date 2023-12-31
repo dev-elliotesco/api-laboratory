28974a67f19c7e3b08e05551ff494dd11207941e
Elliot Escovitch <dev.elliot.escovitch@gmail.com>
2023-12-31 14:35:54 -0500

    update sonar plugin version

--- src/test/java/com/laboratory/application/usecase/ExamUseCaseTest.java
+++ src/test/java/com/laboratory/application/usecase/ExamUseCaseTest.java
@@ -14,9 +14,8 @@ import org.mockito.MockitoAnnotations;
 
 import java.util.Collections;
 import java.util.List;
-
-import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
